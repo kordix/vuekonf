7d118fbf5dd1b26d5daf97fddf2f388bc68d2e62
Kordian <kordian.bober@wp.pl>
2019-06-25 11:20:09 +0200

    images

--- resources/views/vuetest2.blade.php
+++ resources/views/vuetest2.blade.php
@@ -5,14 +5,20 @@
 
 <div class="" id="app">
 <button type="button" name="button" v-for="item in dane2" v-on:click="setscroller(item)">@{{item.nazwa}}</button>
-<div class="scroller">
+<div>
+  <transition-group class="scroller" name="list" tag="span">
+  <div v-for="item in dane2.find((el)=>el.current==true).dane" v-bind:key="item"   class="col-md-3" v-bind:class="{active:item.current}" v-on:click="handleitemclick(item,dane2.find((el)=>el.current==true).dane)">
+      <b><p style="text-align:center;margin-bottom:0px">@{{item.bez}}</p></b>
+      {{-- <img :src="'images/'+dane2.find((el)=>el.current==true).nazwa+'/'+item.artnr+'.png' " alt=""> --}}
+      <img class="img-fluid" :src="'images/'+dane2.find((el)=>el.current==true).nazwa+'/'+item.artnr+'.png' " >
+     {{-- <img class="img-fluid" src="{{asset('./images/test.png' )}}"> --}}
 
-  <div v-for="item in dane2.find((el)=>el.current==true).dane"  class="col-md-3">
-  <p v-bind:class="{active:item.current}" v-on:click="handleitemclick(item,dane2.find((el)=>el.current==true).dane)" >@{{item.bez}}</p>
+  {{-- <p v-bind:class="{active:item.current}" v-on:click="handleitemclick(item,dane2.find((el)=>el.current==true).dane)" >@{{item.bez}}</p> --}}
 
   </div>
+  </transition-group>
 </div>
-  
+
 
 </div>
 </div>
